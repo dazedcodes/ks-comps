0e6ee74580037f0661b9283d1d8c977e3b957a17
Maya Murphy <murphym2@carleton.edu>
2020-11-04 20:31:39 -0600

    BUGCATCH: input box wouldn't accept characters --> keydown event was nested in if else statement with no else

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ document.getElementById("pw-input").addEventListener("click", (e) => {
   event.preventDefault();
 });
 
-document.getElementById("pw-input").addEventListener('keydown', (e) => {
-  if (event.code == 'Backspace'){
-    window.alert("You pressed the backspace key. Try typing password1234 WITHOUT pressing the backspace.");
+let counter = 0;
+document.getElementById("add-btn").addEventListener("click", (e) => {
+    counter = counter + 1;
+    console.log('Hey user! You clicked the submit button.');
     document.getElementById("pw-input").value = "";
-    console.log('The physical key pressed was the BACKSPACE key.');
-  }
+    document.getElementById("counter").innerHTML = "Round: " + counter;
+
 });
 
 document.getElementById("pw-input").addEventListener('keydown', (e) => {
@@ -37,12 +38,10 @@ document.getElementById("pw-input").addEventListener('keydown', (e) => {
     console.log('The physical key pressed was the Enter key.');
     event.preventDefault();
   }
-});
-let counter = 0;
-document.getElementById("add-btn").addEventListener("click", (e) => {
-    counter = counter + 1;
-    console.log('Hey user! You clicked the submit button.');
+  if (event.code == 'Backspace'){
+    window.alert("You pressed the backspace key. Try typing password1234 WITHOUT pressing the backspace.");
     document.getElementById("pw-input").value = "";
-    document.getElementById("counter").innerHTML = "Round: " + counter;
+    console.log('The physical key pressed was the BACKSPACE key.');
+  }
 
 });
