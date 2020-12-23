37593caa4adc24e29493331a9f3207c8e0364107
Nathaniel Reid <nathanielereid107@gmail.com>
2020-12-22 20:50:35 -0600

    Firebase data logging draft 1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 document.getElementById("pw-input").addEventListener("click", (e) => {
   console.log("Hey user! You clicked the password text field.");
   event.preventDefault();
@@ -10,29 +9,24 @@ $(document).ready(function(){
     });
 });
 
+let uuid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 let roundCounter = 0;
 let testPasscode = document.getElementById("passcode-wrapper").innerHTML;
-let ksData = [
-  {
-    "uuid": 1,
-    "round": 1,
-    "character": "A",
-    "keydown-mili": 0065,
-    "keyup-mili": 0080
-  }
-]
-console.log(ksData);
+let ksDataHolding = [];
+
 document.getElementById("add-btn").addEventListener("click", (e) => {
     if (document.getElementById("pw-input").value == "password1234") {
       roundCounter = roundCounter + 1;
       console.log('Hey user! You clicked the submit button.');
       document.getElementById("pw-input").value = "";
       document.getElementById("counter").innerHTML = "Round: " + roundCounter;
+      writeKsData();
     }
     else {
       window.alert('Hey user! You clicked the submit button, but your password input does not match our request! Please try again!');
       document.getElementById("pw-input").value = "";
       console.log('Disregard all inputs of Round' + roundCounter + 'above this mark.');
+      ksDataHolding = [];
     }
 });
 
@@ -41,7 +35,6 @@ document.getElementById("pw-input").addEventListener('keydown', (e) => {
   let character_ascii = (event.keyCode);
   var flight_time = Date.now();
   let round = roundCounter;
-  var database
   if (event.code == 'Enter'){
     console.log('The physical key pressed was the Enter key.');
     event.preventDefault();
@@ -49,18 +42,12 @@ document.getElementById("pw-input").addEventListener('keydown', (e) => {
     window.alert("You pressed the backspace key. Try typing password1234 WITHOUT pressing the backspace.");
     document.getElementById("pw-input").value = "";
     console.log('The physical key pressed was the BACKSPACE key.');
-  //} else if (document.getElementById("pw-input").value ==
-       //     testPasscode.substring(0, document.getElementById("pw-input").value.length - 1)){
-    //window.alert("You have made a typo while inserting the passcode. We've all done it. Many times. Try again!");
-    //document.getElementById("pw-input").value = "";
-  //  console.log('The physical key pressed was a typo.');
+    ksDataHolding = [];
   } else {
-    // console.log(character);
     console.log("ASCII value of", character, "(keydown): ", character_ascii);
     console.log("Miliseconds of", character, "(keydown): ", flight_time);
     console.log("Round of", character, ": ", round);
-    // character-keydown-mili == flight_time
-    // character-round == round
+    stashKsData(uuid, round, character, "keydown")
   }
 });
 
@@ -76,17 +63,36 @@ document.getElementById("pw-input").addEventListener("keyup", (e) => {
     window.alert("You pressed the backspace key. Try typing password1234 WITHOUT pressing the backspace.");
     document.getElementById("pw-input").value = "";
     console.log('The physical key pressed was the BACKSPACE key.');
-  //} else if (document.getElementById("pw-input").value ==
-    //        testPasscode.substring(0, document.getElementById("pw-input").value.length)){
-    //window.alert("You have made a typo while inserting the passcode. We've all done it. Many times. Try again!");
-    //document.getElementById("pw-input").value = "";
-    //console.log('The physical key pressed was a typo.');
+    ksDataHolding = [];
   } else {
     console.log(character);
     console.log("ASCII value of", character, "(keyup): ", character_ascii);
     console.log("Miliseconds of", character, "(keyup): ", dwell_time);
     console.log("Round of", character, ": ", round);
-    // character-keyup-mili == dwell_time
-    // character-round == round
+    stashKsData(uuid, round, character, "keyup")
   }
 });
+
+function writeKsData() {
+  for(let i = 0; i < ksDataHolding.length; i++){
+    firebase.database().ref('events/' + ksDataHolding[i].uuid + '/round' + ksDataHolding[i].round + '/' + ksDataHolding[i].character + '/' + ksDataHolding[i].time).set({
+      uuid: ksDataHolding[i].uuid,
+      round: ksDataHolding[i].round,
+      character: ksDataHolding[i].character,
+      time: ksDataHolding[i].time,
+      eventType: ksDataHolding[i].eventType
+    });
+  }
+  ksDataHolding = []
+}
+
+function stashKsData(uuid, round, character, eventType) {
+  let ksData =  {
+    "uuid": uuid,
+    "round": round,
+    "character": character,
+    "time": Date.now(),
+    "eventType": eventType
+    }
+  ksDataHolding.push(ksData);
+}
